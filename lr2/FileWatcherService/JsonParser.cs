e917fb7ff2dcad5d6151140079a3c32bc957b491
polinasevko <polinasevko24@gmailcom>
2020-11-30 23:47:14 +0300

    this is 3rd lab
    this is 3rd lab

---
+++ lr2/FileWatcherService/JsonParser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.Json;
+
+namespace FileWatcherService
+{
+    class JsonParser<T> : IConfigParser<T> where T : class
+    {
+        private readonly string jsonPath;
+
+        public JsonParser(string jsonPath)
+        {
+            this.jsonPath = jsonPath;
+        }
+
+        public T Parse()
+        {
+            using (FileStream fileStream = new FileStream(jsonPath, FileMode.OpenOrCreate))
+            {
+                using (JsonDocument document = JsonDocument.Parse(fileStream))
+                {
+                    JsonElement element = document.RootElement;
+                    if (typeof(T).GetProperties().First().Name != element.EnumerateObject().First().Name)
+                    {
+                        element = element.GetProperty(typeof(T).Name);
+                    }
+                    try
+                    {
+                        return JsonSerializer.Deserialize<T>(element.GetRawText());
+                    }
+                    catch (Exception ex)
+                    {
+                        using (var streamWriter = new StreamWriter(@"C:\Users\polin\source\repos\3_sem\lr2\FileWatcherService\error.txt", true, Encoding.Default))
+                        {
+                            streamWriter.WriteLine("Error of json file deserialization: " + ex.Message);
+                        }
+                        return null;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ lr2/FileWatcherService/Service1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.ServiceProcess;
+using System.IO;
+using System.Threading;
+
+namespace FileWatcherService
+{
+    public partial class Service1 : ServiceBase
+    {
+        Logger logger;
+        public Service1()
+        {
+            InitializeComponent();
+            this.CanStop = true;
+            this.CanPauseAndContinue = true;
+            this.AutoLog = true;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            logger = new Logger();
+            Thread loggerThread = new Thread(new ThreadStart(logger.Start));
+            loggerThread.Start();
+        }
+
+        protected override void OnStop()
+        {
+            logger.Stop();
+            Thread.Sleep(1000);
+        }
+    }
+
+    class Logger
+    {
+        private readonly Options configOptions;
+        bool enabled = true;
+        object obj = new object();
+        FileSystemWatcher watcher;
+
+        public Logger()
+        {
+            ConfigManager optionsManager = new ConfigManager(@"C:\Users\polin\source\repos\3_sem\lr2\FileWatcherService");
+            configOptions = optionsManager.GetOptions<Options>();
+            CreateDir(configOptions.StorageOptions.SourceDir);
+            CreateDir(configOptions.StorageOptions.TargetDir);
+            watcher = new FileSystemWatcher(configOptions.StorageOptions.SourceDir);
+            watcher.Deleted += Watcher_Deleted;
+            watcher.Created += Watcher_Created;
+            watcher.Changed += Watcher_Changed;
+            watcher.Renamed += Watcher_Renamed;
+        }
+
+        public void Start()
+        {
+            watcher.EnableRaisingEvents = true;
+            while (enabled)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+        public void Stop()
+        {
+            watcher.EnableRaisingEvents = false;
+            enabled = false;
+        }
+        // создание директорий 
+        private void CreateDir(string dirName)
+        {
+            if (Directory.Exists(dirName))
+            {
+                return;
+            }
+            Directory.CreateDirectory(dirName);
+        }
+        // удаление файлов
+        private void Watcher_Deleted(object sender, FileSystemEventArgs e)
+        {
+            string fileEvent = "удален";
+            string filePath = e.FullPath;
+            RecordEntry(fileEvent, filePath);
+        }
+        // переименование файлов
+        private void Watcher_Renamed(object sender, RenamedEventArgs e)
+        {
+            string fileEvent = "переименован в " + e.FullPath;
+            string filePath = e.OldFullPath;
+            RecordEntry(fileEvent, filePath);
+        }
+        // изменение файлов
+        private void Watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            string fileEvent = "изменен";
+            string filePath = e.FullPath;
+            RecordEntry(fileEvent, filePath);
+        }
+        // создание файлов
+        private void Watcher_Created(object sender, FileSystemEventArgs e)
+        {
+            string fileEvent = "создан";
+            string filePath = e.FullPath;
+            string fileName = e.Name;
+
+            if (filePath.Contains(".gz") || filePath.Contains(".crypt"))
+            {
+                return;
+            }
+
+            RecordEntry(fileEvent, filePath);
+
+            string compressedFile = Path.ChangeExtension(filePath, "gz");
+            string newCompressedFile = configOptions.StorageOptions.TargetDir + "\\" + Path.ChangeExtension(fileName, "gz");
+            string targetFile = Path.ChangeExtension(newCompressedFile, "crypt");
+
+            string encryptFile = Actions.Encryption(filePath, true, configOptions.CryptingOptions.Key);
+
+            Actions.Compress(encryptFile, compressedFile);
+            File.Delete(filePath);
+
+            File.Move(compressedFile, newCompressedFile);
+            
+            Actions.Decompress(newCompressedFile, targetFile);
+
+            filePath = Actions.Encryption(targetFile, false, configOptions.CryptingOptions.Key);
+
+            Actions.AddToArchive(filePath, configOptions.ArchiveOptions.ArchName);
+
+            File.Delete(encryptFile);
+            File.Delete(newCompressedFile);
+            File.Delete(targetFile);
+        }
+
+        private void RecordEntry(string fileEvent, string filePath, string fileName = null)
+        {
+            lock (obj)
+            {
+                using (StreamWriter writer = new StreamWriter(@"C:\Users\polin\source\repos\3_sem\lr2\templog.TXT", true))
+                {
+                    writer.WriteLine(String.Format("{0} файл {1} был {2}",
+                        DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss"), filePath, fileEvent));
+                    writer.Flush();
+                }
+            }
+        }
+    }
+}
