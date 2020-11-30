0d994b71b4fc2e8a2828f2b6fde4a466ff4ff5b1
polinasevko <61377437+polinasevko@users.noreply.github.com>
2020-11-30 23:37:11 +0300

    Add files via upload

---
+++ lr3/Actions.cs
@@ -0,0 +1,77 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace FileWatcherService
+{
+    class Actions
+    {
+        public static string Encryption(string FilePath, bool mode, string key)
+        {
+            if (key == "12345678")
+            {
+                StringBuilder temp = new StringBuilder(FilePath);
+                if (!mode)
+                {
+                    temp.Replace(".crypt", ".TXT");
+                }
+                byte[] curFile = File.ReadAllBytes(FilePath);
+                byte[] newFile = Crypt(curFile);
+                if (mode)
+                {
+                    temp.Replace(".TXT", ".crypt");
+                }
+                File.WriteAllBytes(temp.ToString(), newFile);
+                return temp.ToString();
+            }
+            else
+            {
+                return FilePath;
+            }
+
+        }
+
+        private static byte[] Crypt(byte[] bytes)
+        {
+            for (int i = 0; i < bytes.Length; i++)
+                bytes[i] ^= 1;
+            return bytes;
+        }
+
+        public static void Compress(string sourceFile, string compressedFile)
+        {
+            using (FileStream sourceStream = new FileStream(sourceFile, FileMode.Open))
+            {
+                using (FileStream targetStream = File.Create(compressedFile))
+                {
+                    using (GZipStream compressionStream = new GZipStream(targetStream, CompressionMode.Compress))
+                    {
+                        sourceStream.CopyTo(compressionStream);
+                    }
+                }
+            }
+        }
+
+        public static void Decompress(string compressedFile, string targetFile)
+        {
+            using (FileStream sourceStream = new FileStream(compressedFile, FileMode.Open))
+            {
+                using (FileStream targetStream = File.Create(targetFile))
+                {
+                    using (GZipStream decompressionStream = new GZipStream(sourceStream, CompressionMode.Decompress))
+                    {
+                        decompressionStream.CopyTo(targetStream);
+                    }
+                }
+            }
+        }
+
+        public static void AddToArchive(string filePath, string myArchive)
+        {
+            using (ZipArchive archive = ZipFile.Open(myArchive, ZipArchiveMode.Update))
+            {
+                archive.CreateEntryFromFile(filePath, Path.GetFileName(filePath));
+            }
+        }
+    }
+}
---
+++ lr3/XmlParser.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace FileWatcherService
+{
+    class XmlParser<T> : IConfigParser<T> where T : class
+    {
+        private readonly string xmlPath = null;
+        private readonly string xsdPath = null;
+
+        public XmlParser(string xmlPath)
+        {
+            this.xmlPath = xmlPath;
+            if (File.Exists(Path.ChangeExtension(xmlPath, "xsd")))
+            {
+                xsdPath = Path.ChangeExtension(xmlPath, "xsd");
+            }
+        }
+
+        public T Parse()
+        {
+            if (xsdPath != null && !Validate(xmlPath, xsdPath))
+            {
+                return null;
+            }
+
+            try
+            {
+                XDocument xDocument = XDocument.Load(xmlPath);
+                var elements =
+                    from element in xDocument.Elements(typeof(T).Name).DescendantsAndSelf()
+                    select element;
+
+                string xmlFormat = elements.First().ToString();
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+
+                using (TextReader textReader = new StringReader(xmlFormat))
+                {
+                    return xmlSerializer.Deserialize(textReader) as T;
+                }
+            }
+            catch (Exception ex)
+            {
+                using (var streamWriter = new StreamWriter(@"C:\Users\polin\source\repos\3_sem\lr2\FileWatcherService\error.txt", true, Encoding.Default))
+                {
+                    streamWriter.WriteLine("Error of xml file deserialization: " + ex.Message);
+                }
+                return null;
+            }
+        }
+
+        private bool Validate(string xmlPath, string xsdPath)
+        {
+            try
+            {
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.ValidationType = ValidationType.Schema;
+                settings.Schemas.Add(null, XmlReader.Create(xsdPath));
+
+                XmlReader xmlReader = XmlReader.Create(xmlPath, settings);
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.Load(xmlReader);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                using (var streamWriter = new StreamWriter(@"C:\Users\polin\source\repos\3_sem\lr2\FileWatcherService\error.txt", true, Encoding.Default))
+                {
+                    streamWriter.WriteLine("Error of validation: " + ex.Message);
+                }
+                return false;
+            }
+        }
+    }
+}
